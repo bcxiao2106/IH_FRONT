26699e9ca0e5b2c8f5a61e1ca25a0d2b14d1c554
bichengxiao16@gmail.com <bichengxiao16@gmail.com>
2019-03-05 19:05:41 -0500

    Added console.log port

--- server.js
+++ server.js
@@ -9,5 +9,8 @@ app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/IntvHelper/index.html'));
 });
 
+var port = process.env.PORT || 8888;
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8888);
+app.listen(port, () => {
+    console.log('App is running on port ' + port);
+});
