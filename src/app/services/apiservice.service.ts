712f554a8906580fcb9a78183156650aea10e452
bichengxiao16@gmail.com <bichengxiao16@gmail.com>
2019-04-15 22:47:06 -0400

    Fixed answers and questions

--- src/app/services/apiservice.service.ts
+++ src/app/services/apiservice.service.ts
@@ -14,12 +14,12 @@ import { environment } from './../../environments/enviornment.dev';
 export class ApiserviceService {
 
   private webApiHostURL = 'https://groupint.herokuapp.com/';
-  private getQListURL = this.webApiHostURL + 'Questions';
-  private getCatListURL = this.webApiHostURL + 'Categories';
-  private getSolutionListURL = this.webApiHostURL + 'Answers';
-  private postNewQuestionURL = this.webApiHostURL + 'api/NewQuestion';
+  private getQListURL = this.webApiHostURL + 'questions';
+  private getCatListURL = this.webApiHostURL + 'categories';
+  private getSolutionListURL = this.webApiHostURL + 'answers';
+  private postNewQuestionURL = this.webApiHostURL + 'questions';
   private putQuestionChangeURL = this.webApiHostURL + '';
-  private postNewSolutionURL = this.webApiHostURL + 'api/NewSolution';
+  private postNewSolutionURL = this.webApiHostURL + 'answers';
 
   constructor(private httpClient: HttpClient,
     private messageService: MessageService) {
